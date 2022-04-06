3155c9c44edf9b2cd2b2d7d63a5ac574d2ec16cc
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-06 12:58:17 +0530

    using route for few el in DataAPI file to lift up the state

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import 'normalize.css';
-import RecipeContainer from "./components/RecipeContainer"
+import "normalize.css";
+import RecipeContainer from "./components/RecipeContainer";
 import RecipeDetail from "./components/RecipeDetail";
 import Bookmark from "./components/Bookmark";
+import DataAPI from "./components/DataAPI";
 
 ReactDOM.render(
   <Router>
     <Layout>
+      <DataAPI />
       <Routes>
-        <Route path="search/:id" element={<RecipeContainer/>}/>
-        <Route path="search/:id/id=:id" element={<RecipeDetail/>}/>
-        <Route path="/bookmark" element={<Bookmark/>} />
+        <Route path="search/:id" element={<RecipeContainer />} />
+        {/* <Route path="search/:id/id=:id" element={<RecipeDetail />} />
+        <Route path="/bookmark" element={<Bookmark/>} /> */}
       </Routes>
     </Layout>
   </Router>,
