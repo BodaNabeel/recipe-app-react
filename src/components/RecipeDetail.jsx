a9722534e4a427415660bc4a1da71054e4541dfa
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-07 11:09:13 +0530

    code formated

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -5,7 +5,7 @@ import SyncLoader from "react-spinners/SyncLoader";
 import { BsBookmarkFill } from "react-icons/bs";
 import { v4 as uuidv4 } from "uuid";
 
-export default function RecipeDetail({ setRecipeData }) {
+export default function RecipeDetail({ setRecipeData}) {
   const location = useLocation();
   const id = location.state;
   const API_URL = "https://forkify-api.herokuapp.com/api/get?rId=";
