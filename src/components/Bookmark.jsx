31265da8f5704ad210b79d14c1b1b91ea3215335
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-07 23:31:16 +0530

    bookmark component markup added (beta)

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Bookmark() {
-  return (
-    <div>BOOKMARK ROUTE YAHOOO!!</div>
-  )
+export default function Bookmark({ bookmarks }) {
+  const displayBookmarks = (data) => {
+    if (data.length > 0) {
+      return (
+        <div className="bookmark-container">
+          {data.map((element) => {
+            return (
+              <div key={uuidv4()} className="bookmark-card">
+                <div className="bookmark-img"></div>
+                <div className="bookmark-content">
+                  <h1 className="bookmark-recipeName">{element.title}</h1>
+                  <p className="bookmark-publisher">{element.publisher}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return <h1 className="bookmark-title">NO BOOKMARK ADDED</h1>;
+    }
+  };
+  displayBookmarks(bookmarks);
+  return displayBookmarks(bookmarks);
 }
