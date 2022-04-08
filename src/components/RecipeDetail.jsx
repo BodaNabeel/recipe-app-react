edfa7d7a5aa027fa527716de9023bd6ed945ada2
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-08 16:37:47 +0530

    changing the styled of bookmark dynamically function added

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -5,7 +5,12 @@ import SyncLoader from "react-spinners/SyncLoader";
 import { BsBookmarkFill } from "react-icons/bs";
 import { v4 as uuidv4 } from "uuid";
 
-export default function RecipeDetail({ setRecipeData, recipeData }) {
+export default function RecipeDetail({
+  setRecipeData,
+  recipeData,
+  bookmarkIDs,
+  setBookmarkIDs,
+}) {
   const location = useLocation();
   const id = location.state;
   const API_URL = "https://forkify-api.herokuapp.com/api/get?rId=";
@@ -15,29 +20,39 @@ export default function RecipeDetail({ setRecipeData, recipeData }) {
 
   // toggling bookmark
   const toggleBookmark = () => {
-    setIsActive(!isActive);
-    let recipe_ids = [];
+    const recipe_ids = [];
     recipeData.forEach((element) => {
       recipe_ids.push(element.recipe_id);
     });
-
     // prevetning a recipe to be stored more than once
     if (
       detailedRecipe !== "" &&
       !recipe_ids.includes(detailedRecipe.recipe_id)
     ) {
+      setIsActive(true);
       setRecipeData((prevState) => {
         return [...prevState, detailedRecipe];
       });
-    } 
+      setBookmarkIDs((prevState) => {
+        return [...prevState, detailedRecipe.recipe_id];
+      });
+    }
     // removing item from bookmark
     else if (recipe_ids.includes(detailedRecipe.recipe_id)) {
+      // setIsActive(false);
       recipe_ids.forEach((el) => {
         const updatedList = recipeData.filter(
           (value) => value.recipe_id !== el
         );
         console.log(updatedList);
         setRecipeData(updatedList);
+      });
+
+      bookmarkIDs.forEach((el) => {
+        const updatedBookmark = recipeData.filter(
+          (value) => value.recipe_id !== el
+        );
+        setBookmarkIDs(updatedBookmark);
       });
     }
   };
@@ -55,9 +70,10 @@ export default function RecipeDetail({ setRecipeData, recipeData }) {
       <div className="detail-container">
         <div className="detail-top">
           <BsBookmarkFill
+            key={uuidv4}
             onClick={toggleBookmark}
             className={
-              isActive
+              bookmarkIDs.includes(data.recipe_id)
                 ? "bookmark-icon bookmark-icon_active"
                 : "bookmark-icon bookmark-icon_passive"
             }
